b5505f616f427a38410cc58071f6c549b8cf2909
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2025-10-02 16:30:56 -0300

    criei o modelo Livro

--- src/main/java/com/pedro/biblion/model/Livro.java
+++ src/main/java/com/pedro/biblion/model/Livro.java
@@ -1,4 +1,40 @@
 package com.pedro.biblion.model;
 
 public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private String status;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
 }
